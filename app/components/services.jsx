80e3e59ad3e64a64c34b009596e16be86808fcd8
Thatikonda Mukesh <thatikondamukesh08@gmail.com>
2024-12-17 11:07:56 +0530

    Changes in Testimonals

--- app/components/services.jsx
+++ app/components/services.jsx
@@ -4,12 +4,47 @@ import 'aos/dist/aos.css';
 import AOS from 'aos';
 
 const Services = () => {
+    const [hoveredIndex, setHoveredIndex] = React.useState(null);
+
     useEffect(() => {
         AOS.init({
             duration: 1000, // Animation duration in ms
             once: true, // Run animation only once
         });
     }, []);
+
+    const services = [
+        {
+            img: '/logos/expert.png',
+            title: 'Our Expertise',
+            description: 'Comprehensive Solutions for Packaging and Machinery Excellence',
+        },
+        {
+            img: '/logos/design.png',
+            title: 'Human-Centric Machine Design',
+            description: 'Creating Solutions for Packaging and Machinery Excellence',
+        },
+        {
+            img: '/logos/innovate.png',
+            title: 'Innovative Machine Interfaces',
+            description: 'Designing User-Friendly Control Systems for Seamless Workflow',
+        },
+        {
+            img: '/logos/package.png',
+            title: 'Packaging and Marketing Assistance',
+            description: 'Helping Clients Effectively Market Products with Advanced Machinery',
+        },
+        {
+            img: '/logos/view.png',
+            title: 'Digital Presence Enhancement',
+            description: 'Optimizing Online Visibility for Packaging Businesses',
+        },
+        {
+            img: '/logos/monitor.png',
+            title: 'Smart Monitoring Tools',
+            description: 'Leveraging Android Apps for Enhanced Machinery Control',
+        },
+    ];
 
     return (
         <div className="container mx-auto p-8 flex flex-col items-center mb-8">
@@ -26,79 +61,28 @@ const Services = () => {
                 <br />
                 deliver efficient, reliable, and cost-effective solutions to streamline your packaging processes.
             </p>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" style={{width: '80%'}}>
-                <div
-                    data-aos="fade-up"
-                    className="bg-white p-6 shadow-lg text-left relative transform transition duration-500 hover:scale-105 hover:shadow-2xl"
-                >
-                    <img src="\logos\expert.png" alt="" className="w-10 pt-12 pb-2" />
-                    <hr className="border-gray-300 my-4" />
-                    <h2 className="text-2xl font-semibold mb-2 pt-0">Our Expertise</h2>
-                    <p className="text-gray-700 pb-12" style={{ color: '#7F7F7F',width: '85%' }}>
-                        Comprehensive Solutions for Packaging and Machinery Excellence
-                    </p>
-                    <div className="absolute bottom-0 left-0 w-full h-2 bg-green-500"></div>
-                </div>
-                <div
-                    data-aos="fade-up"
-                    className="bg-white p-6 shadow-lg text-left relative transform transition duration-500 hover:scale-105 hover:shadow-2xl"
-                >
-                    <img src="\logos\design.png" alt="" className="w-10 pt-12 pb-2" />
-                    <hr className="border-gray-300 my-4" />
-                    <h2 className="text-2xl font-semibold mb-2 pt-0">Human-Centric Machine Design</h2>
-                    <p className="text-gray-700 pb-12" style={{ color: '#7F7F7F', width: '85%' }}>
-                        Creating Solutions for Packaging and Machinery Excellence
-                    </p>
-                    <div className="absolute bottom-0 left-0 w-full h-2 bg-green-500"></div>
-                </div>
-                <div
-                    data-aos="fade-up"
-                    className="bg-white p-6 shadow-lg text-left relative transform transition duration-500 hover:scale-105 hover:shadow-2xl"
-                >
-                    <img src="\logos\innovate.png" alt="" className="w-10 pt-12 pb-2" />
-                    <hr className="border-gray-300 my-4" />
-                    <h2 className="text-2xl font-semibold mb-2 pt-0">Innovative Machine Interfaces</h2>
-                    <p className="text-gray-700 pb-12" style={{ color: '#7F7F7F', width: '85%' }}>
-                        Designing User-Friendly Control Systems for Seamless Workflow
-                    </p>
-                    <div className="absolute bottom-0 left-0 w-full h-2 bg-green-500"></div>
-                </div>
-                <div
-                    data-aos="fade-up"
-                    className="bg-white p-6 shadow-lg text-left relative transform transition duration-500 hover:scale-105 hover:shadow-2xl"
-                >
-                    <img src="\logos\package.png" alt="" className="w-10 pt-12 pb-2" />
-                    <hr className="border-gray-300 my-4" />
-                    <h2 className="text-2xl font-semibold mb-2 pt-0">Packaging and Marketing Assistance</h2>
-                    <p className="text-gray-700 pb-12" style={{ color: '#7F7F7F', width: '85%' }}>
-                        Helping Clients Effectively Market Products with Advanced Machinery
-                    </p>
-                    <div className="absolute bottom-0 left-0 w-full h-2 bg-green-500"></div>
-                </div>
-                <div
-                    data-aos="fade-up"
-                    className="bg-white p-6 shadow-lg text-left relative transform transition duration-500 hover:scale-105 hover:shadow-2xl"
-                >
-                    <img src="\logos\view.png" alt="" className="w-10 pt-12 pb-2" />
-                    <hr className="border-gray-300 my-4" />
-                    <h2 className="text-2xl font-semibold mb-2 pt-0">Digital Presence Enhancement</h2>
-                    <p className="text-gray-700 pb-12" style={{ color: '#7F7F7F', width: '85%' }}>
-                        Optimizing Online Visibility for Packaging Businesses
-                    </p>
-                    <div className="absolute bottom-0 left-0 w-full h-2 bg-green-500"></div>
-                </div>
-                <div
-                    data-aos="fade-up"
-                    className="bg-white p-6 shadow-lg text-left relative transform transition duration-500 hover:scale-105 hover:shadow-2xl"
-                >
-                    <img src="\logos\monitor.png" alt="" className="w-10 pt-12 pb-2" />
-                    <hr className="border-gray-300 my-4" />
-                    <h2 className="text-2xl font-semibold mb-2 pt-0">Smart Monitoring Tools</h2>
-                    <p className="text-gray-700 pb-12" style={{ color: '#7F7F7F', width: '85%' }}>
-                        Leveraging Android Apps for Enhanced Machinery Control
-                    </p>
-                    <div className="absolute bottom-0 left-0 w-full h-2 bg-green-500"></div>
-                </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" style={{ width: '80%' }}>
+                {services.map((service, index) => (
+                    <div
+                        key={index}
+                        data-aos="fade-up"
+                        className="bg-white p-6 text-left relative transform transition duration-500 hover:scale-105"
+                        style={{
+                            boxShadow: '5px 15px 15px rgba(0, 0, 0, 0.2)', 
+                            transition: 'box-shadow 0.5s ease, transform 0.5s ease', 
+                        }}
+                        onMouseEnter={() => setHoveredIndex(index)}
+                        onMouseLeave={() => setHoveredIndex(null)}
+                    >
+                        <img src={service.img} alt="" className="w-10 pt-12 pb-2" />
+                        <hr className="border-gray-300 my-4" />
+                        <h2 className="text-2xl font-semibold mb-2 pt-0">{service.title}</h2>
+                        <p className="text-gray-700 pb-12" style={{ color: '#7F7F7F', width: '85%' }}>
+                            {service.description}
+                        </p>
+                        <div className="absolute bottom-0 left-0 w-full h-2 bg-green-500"></div>
+                    </div>
+                ))}
             </div>
         </div>
     );
--- app/components/testimonies.jsx
+++ app/components/testimonies.jsx
@@ -1,23 +1,73 @@
-import React from 'react';
-import PricingCard from './pricing';
+import React, { useState, useEffect } from 'react';
+
 const Testimonies = () => {
-    return (
-        <>
-            <div className="relative bg-cover bg-center h-screen justify-center" style={{ backgroundImage: "url('/testimonials_background.png')" }}>
-                <div className="absolute inset-0 bg-white opacity-90"></div>
-                <div className="relative z-10 flex flex-col items-center  h-full text-black">
-                    <h1 className="text-4xl font-bold mb-4 pt-20" style={{ color: '#4DBF38' }}>Testimonials</h1>
-                    <img src="/logos/testimonies.png" alt="" className='w-10 pt-10' />
-                    <p className="text-lg justify-center text-center pt-4" style={{ color: '#7F7F7F' }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum  <br />
-                        has been the industry's standard dummy text ever since the 1500s, when an unknown <br />
-                        printer took a galley of type and scrambled it to make a type specimen book.
-                    </p>
-                    <h2 className="text-lg font-bold justify-center text-center pt-4 " style={{ color: '#052A47' }}>CRAIG</h2>
-                    <p className="justify-center text-center pt-" style={{ color: '#7F7F7F' }}>Bussiness man</p>
-                </div>
-            </div>
-        </>
-    );
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [fade, setFade] = useState(true);
+
+  const testimonies = [
+    {
+      id: 1,
+      text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      name: "CRAIG",
+      role: "Businessman",
+    },
+    {
+      id: 2,
+      text: "This is an amazing product! It has significantly improved our productivity and workflow. Highly recommended for businesses of all sizes. With its intuitive design and robust features, it allows users to accomplish tasks more efficiently. I was particularly impressed with the attention to detail and the customer support, which ensured we had a seamless experience. This product is truly a testament to excellent craftsmanship and innovation.",
+      name: "SARAH",
+      role: "Entrepreneur",
+    },
+    {
+      id: 3,
+      text: "A game-changer in the industry! The functionality and design are top-notch. Truly a remarkable innovation. It's not just a product; it's a solution that addresses real-world problems effectively. We've seen a dramatic increase in productivity and a decrease in operational costs since adopting it. The team behind this is clearly committed to quality and excellence, making it an essential part of any modern business strategy.",
+      name: "JAMES",
+      role: "CEO",
+    },
+  ];
+  
+
+  const nextSlide = () => {
+    setFade(false);
+    setTimeout(() => {
+      setCurrentSlide((prev) => (prev + 1) % testimonies.length);
+      setFade(true);
+    }, 200);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 8000); // Automatically change slides every 8 seconds
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="relative bg-cover bg-center h-screen justify-center" style={{ backgroundImage: "url('/testimonials_background.png')" }}>
+      <div className="absolute inset-0 bg-white opacity-90"></div>
+      <div className="relative z-10 flex flex-col items-center h-full text-black">
+        <h1 className="text-4xl font-bold mb-4 pt-20" style={{ color: '#4DBF38' }}>Testimonials</h1>
+        <img src="/logos/testimonies.png" alt="Testimonials Logo" className="w-10 pt-10" />
+        <div className="relative h-full w-1/2 flex flex-col items-center justify-start mt-8">
+          <div className={`transition-opacity duration-1000 ${fade ? 'opacity-100' : 'opacity-0'}`}>
+            <p className="text-lg text-center pt-4 h-56" style={{ color: '#7F7F7F' }}>
+              {testimonies[currentSlide].text}
+            </p>
+            <h2 className="text-lg font-bold text-center pt-4 mt-4" style={{ color: '#052A47' }}>
+              {testimonies[currentSlide].name}
+            </h2>
+            <p className="text-center" style={{ color: '#7F7F7F' }}>{testimonies[currentSlide].role}</p>
+          </div>
+          <div className="w-full flex justify-center mt-8">
+            {testimonies.map((testimony, index) => (
+              <div
+                key={testimony.id}
+                className={`w-3 h-3 mx-2 rounded-full cursor-pointer ${currentSlide === index ? 'bg-[#4DBF38]' : 'bg-gray-300'}`}
+                onClick={() => setCurrentSlide(index)}
+              ></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonies;
